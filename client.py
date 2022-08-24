e5c3446a45c1ebce3e7a999503af957f87e6dffc
kdi <kerwin.dahomey@gmail.com>
2022-08-24 10:56:17 -0500

    fix authentication method

--- client.py
+++ client.py
@@ -21,7 +21,6 @@ class FTXClient:
 
     def _authenticate(self, request: Request):
         ts = int(time.time() * 1000)
-        request = Request('GET', )
         prepared = request.prepare()
         signature_payload = f'{ts}{prepared.method}{prepared.path_url}'.encode(
         )
@@ -29,6 +28,6 @@ class FTXClient:
             signature_payload += prepared.body
         signature = hmac.new(self.secret_key.encode(),
                              signature_payload, 'sha256').hexdigest()
-        prepared.headers[f'FTXUS-KEY'] = self.api_key
-        prepared.headers[f'FTXUS-SIGN'] = signature
-        prepared.headers[f'FTXUS-TS'] = str(ts)
+        request.headers[f'FTXUS-KEY'] = self.api_key
+        request.headers[f'FTXUS-SIGN'] = signature
+        request.headers[f'FTXUS-TS'] = str(ts)
--- trackstar.py
+++ trackstar.py
@@ -3,4 +3,6 @@ from dotenv import dotenv_values
 
 if __name__ == "__main__":
     config = dotenv_values(".env")
-    FTX = FTXClient()
+    FTX = FTXClient(config['KEY'], config['SECRET'])
+    r = FTX._request('GET', '/markets')
+    print(r)
